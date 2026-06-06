fb5513cfeebeb5f01cadc615321590562819a9ed
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-06 23:18:03 +0700

    git commit -m "week07: create ApiResponse sealed class"

--- src/oop_103737_mohammadubaidurrachman/week07/SystemStates.kt
+++ src/oop_103737_mohammadubaidurrachman/week07/SystemStates.kt
@@ -6,3 +6,15 @@ enum class AppState {
     RUNNING,
     STOPPED
 }
+sealed class ApiResponse {
+
+    data class Success(
+        val data: String
+    ) : ApiResponse()
+
+    data class Error(
+        val message: String
+    ) : ApiResponse()
+
+    object Loading : ApiResponse()
+}
