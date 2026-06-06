928afcdacc7f276bb561f5af1c89a4b267504f96
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:36:47 +0700

    git commit -m "week08: create nested classes for Order"

---
+++ src/oop_103737_mohammadubaidurrachman/week08/OrderModels.kt
@@ -0,0 +1,18 @@
+package oop_103737_mohammadubaidurrachman.week08
+
+class City(
+    val name: String
+)
+
+class Address(
+    val city: City?
+)
+
+class DeliveryDetails(
+    val address: Address?
+)
+
+class Order(
+    val deliveryDetails: DeliveryDetails?,
+    val totalPrice: Int?
+)
