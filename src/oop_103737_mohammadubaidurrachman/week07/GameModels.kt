c33b972be9b4bce3bdc2798ca34dbfdcee14c235
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-06 23:56:12 +0700

    git commit -m "week07: (task) create ItemRarity enum"

---
+++ src/oop_103737_mohammadubaidurrachman/week07/GameModels.kt
@@ -0,0 +1,12 @@
+package oop_103737_mohammadubaidurrachman.week07
+
+enum class ItemRarity(
+    val dropChance: Int
+) {
+
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(20),
+    EPIC(5),
+    LEGENDARY(1)
+}
