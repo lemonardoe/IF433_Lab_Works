8015e2bf9dcf1940b3df6b4d3bd6556961500bd6
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:54:15 +0700

    git commit -m "week08: create LegacyJavaAPI class in Java"

---
+++ src/oop_103737_mohammadubaidurrachman/week08/LegacyJavaAPI.java
@@ -0,0 +1,9 @@
+package oop_103737_mohammadubaidurrachman.week08;
+
+public class LegacyJavaAPI {
+
+    public static String fetchServerStatus() {
+
+        return "SERVER_ONLINE_200_OK";
+    }
+}
