63c8e285171c10049637fb52769b5eb5718fa483
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:39:59 +0700

    git commit -m "week08: implement chained safe calls and elvis"

---
+++ src/oop_103737_mohammadubaidurrachman/week08/Main.kt
@@ -0,0 +1,20 @@
+package oop_103737_mohammadubaidurrachman.week08
+
+fun main() {
+
+    println("=== TEST SAFE CALLS & ELVIS ===")
+
+    val emptyOrder = Order(
+        null,
+        null
+    )
+
+    val destination =
+        emptyOrder.deliveryDetails
+            ?.address
+            ?.city
+            ?.name
+            ?: "Kota Tidak Diketahui"
+
+    println("Tujuan pengiriman: $destination")
+}
