892f5852babe0a3345937fa6d31675d9539d8e7a
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:40:44 +0700

    git commit -m "week08: implement safe call with let block"

--- src/oop_103737_mohammadubaidurrachman/week08/Main.kt
+++ src/oop_103737_mohammadubaidurrachman/week08/Main.kt
@@ -17,4 +17,23 @@ fun main() {
             ?: "Kota Tidak Diketahui"
 
     println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+
+    val validOrder = Order(
+        null,
+        250000
+    )
+
+    val receipt =
+        validOrder.totalPrice?.let { price ->
+
+            val tax = price * 0.11
+
+            "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+        }
+
+            ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
 }
