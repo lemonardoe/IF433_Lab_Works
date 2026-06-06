1b40d51cb2294d5e52828054e74a957f406fc4eb
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:06:16 +0700

    git commit -m "week07: (task) implement exhaustive processEvent"

---
+++ src/oop_103737_mohammadubaidurrachman/week07/GameExecutor.kt
@@ -0,0 +1,29 @@
+package oop_103737_mohammadubaidurrachman.week07
+
+fun processEvent(event: BattleState) {
+
+    when(event) {
+
+        is BattleState.MonsterEncounter -> {
+
+            println("Monster muncul: ${event.monsterName}")
+        }
+
+        is BattleState.LootDropped -> {
+
+            println(
+                "Loot didapat: ${event.item.name} (${event.item.rarity})"
+            )
+        }
+
+        is BattleState.GameOver -> {
+
+            println("GAME OVER: ${event.reason}")
+        }
+
+        BattleState.SafeZone -> {
+
+            println("Player berada di Safe Zone.")
+        }
+    }
+}
