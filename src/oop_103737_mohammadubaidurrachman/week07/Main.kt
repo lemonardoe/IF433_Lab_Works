2831c06fc906893f6cc3f8f31463e62306fbb54d
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-06 22:36:06 +0700

    git commit -m "week07: test Singleton and Companion Object in main"

--- src/oop_103737_mohammadubaidurrachman/week07/Main.kt
+++ src/oop_103737_mohammadubaidurrachman/week07/Main.kt
@@ -14,4 +14,14 @@ fun main() {
         NetworkClient.createClient()
 
     client.connect()
+
+    println("\n=== TEST REGULAR CLASS ===")
+
+    val reg1 = RegularUser("Alice", 22)
+
+    val reg2 = RegularUser("Alice", 22)
+
+    println(reg1)
+
+    println("Sama? ${reg1 == reg2}")
 }
