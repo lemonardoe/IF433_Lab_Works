d992085599e9c01175aa845f3ede4179d7b2206c
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 00:04:44 +0700

    git commit -m "week07: (task) create BattleState sealed class hierarchy"

---
+++ src/oop_103737_mohammadubaidurrachman/week07/BattleEvent.kt
@@ -0,0 +1,18 @@
+package oop_103737_mohammadubaidurrachman.week07
+
+sealed class BattleState {
+
+    data class MonsterEncounter(
+        val monsterName: String
+    ) : BattleState()
+
+    data class LootDropped(
+        val item: GameItem
+    ) : BattleState()
+
+    data class GameOver(
+        val reason: String
+    ) : BattleState()
+
+    object SafeZone : BattleState()
+}
