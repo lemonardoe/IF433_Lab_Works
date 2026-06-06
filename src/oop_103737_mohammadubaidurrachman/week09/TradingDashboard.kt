603c60ff874533104b310595e851fdb222c8633c
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-06-07 03:59:59 +0700

    git commit -m "week09: (task) chain sortedByDescending and map for top performers"

--- src/oop_103737_mohammadubaidurrachman/week09/TradingDashboard.kt
+++ src/oop_103737_mohammadubaidurrachman/week09/TradingDashboard.kt
@@ -64,4 +64,15 @@ fun main() {
         closedTrades.filter {
             it.roe <= 0
         }
+    val topPerformersString =
+        winningTrades
+            .sortedByDescending {
+                it.roe
+            }
+            .map {
+
+                "WIN [${it.pair} - ${it.position}]: " +
+                        "+${it.roe}% ROE " +
+                        "(Lev: ${it.leverage}x)"
+            }
 }
